322ac440b23978014ce316ce11e5a452c5534071
pacoveirano <139478119+pacoveirano@users.noreply.github.com>
2023-08-31 17:03:28 -0300

    Add files via upload

--- src/tb.v
+++ src/tb.v
@@ -11,14 +11,14 @@ module tb ();
     end
 
     // wire up the inputs
-    wire  clk;
-    wire  rst_n;
-    wire  ena;
-    wire  clk_2;
-    wire  stb;
-    wire  [2:0] sel;
-    wire  [7:0] uio_in;
-    wire  [7:0] ui_in;
+    reg  clk;
+    reg  rst_n;
+    reg  ena;
+    reg  clk_2;
+    reg  stb;
+    reg  [2:0] sel;
+    reg  [7:0] uio_in;
+    reg  [7:0] ui_in;
 
     wire [7:0] uo_out;
     wire [7:0] uio_out;
@@ -26,8 +26,8 @@ module tb ();
 
     integer	j;
     // Clocks
-    always #18 clk = ~clk;
-    always #7 clk_2 = ~clk_2;
+ //   always #18 clk = ~clk;
+ //   always #7 clk_2 = ~clk_2;
 
     assign ui_in[0] = clk_2;
     assign ui_in[4] = stb;
@@ -44,55 +44,5 @@ module tb ();
         .rst_n      (rst_n)     // not reset
         );
 
-    // this part dumps the trace to a vcd file that can be viewed with GTKWave
-    initial begin
-        clk <= 0;
-        clk_2 <= 0;
-        rst_n  <= 0;
-        ena    <= 0;
-        ui_in  <= 0;
-        uio_in <= 0;
-        stb <= 0;
-        sel <= '0;
-    end
 
-    initial begin
-        #20
-    //Simulación con SEL=0 únicamente registramos la entrada
-        rst_n <= 1;
-        ena   <= 1;
-        sel   <= 0;
-     #3 uio_in <= 01010101;
-        #30;
-    //Simulación con SEL=1, registramos con reloj 2 sin sicnronizar
-        sel <= 1;
-     #3 uio_in <= 11111111;
-        #40
-    //Simulación con SEL=2, registramos con sincro de dos FF en cascada
-        sel <= 2;
-     #3 uio_in <= 00000000;
-        #50
-    //Simulación con SEL=3, registramos con sincro con señal de control
-        sel <= 3;
-     #3 uio_in <= 11111111;
-     #5 stb <= 1;
-    #20 stb <= 0;
-        #50;
-    end
-
-    initial begin
-        #250
-        repeat (2) @(posedge clk);
-        rst_n  <= 1;
-        ena    <= 1;
-        
-        for (j=1; j<=5; j=j+1) begin//
-            repeat (5) @(posedge clk);
-            sel <= $random();
-            stb <= $random();
-            repeat (5) @(posedge clk);
-            uio_in <= $random();
-        end
-        #500 $stop;   // end of simulation;
-    end
 endmodule
--- src/test.py
+++ src/test.py
@@ -2,47 +2,42 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
+# https://docs.cocotb.org/en/stable/quickstart.html
 
-segments = [ 63, 6, 91, 79, 102, 109, 124, 7, 127, 103 ]
+async def generate_clock(dut):
+		for cycle in range(1000):
+				"""Generate clock 2 pulses."""
+				dut.clk.value = 0
+				await Timer(90, units="ns")
+				dut.clk.value = 1
+				await Timer(90, units="ns")	
+	
+	
+async def generate_clock2(dut):
+		for cycle in range(1000):
+				"""Generate clock 2 pulses."""
+				dut.clk_2.value = 0
+				await Timer(40, units="ns")
+				dut.clk_2.value = 1
+				await Timer(40, units="ns")	
+
+async def reset_dut(reset_n, duration_ns):
+		reset_n.value = 0
+		await Timer(duration_ns, units="ns")
+		reset_n.value = 1
+		reset_n._log.debug("Reset complete")
+
 
 @cocotb.test()
-async def test_7seg(dut):
-    dut._log.info("start")
-    clock = Clock(dut.clk, 10, units="us")
-    cocotb.start_soon(clock.start())
-
-    # reset
-    dut._log.info("reset")
-    dut.rst_n.value = 0
-    # set the compare value
-    dut.ui_in.value = 1
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
-
-    # the compare value is shifted 10 bits inside the design to allow slower counting
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, max_count)
-        assert int(dut.segments.value) == segments[i % 10]
-
-        # all bidirectionals are set to output
-        assert dut.uio_oe == 0xFF
-
-    # reset
-    dut.rst_n.value = 0
-    # set a different compare value
-    dut.ui_in.value = 3
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
-
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, max_count)
-        assert int(dut.segments.value) == segments[i % 10]
-
+async def test_my_design(dut):
+		dut._log.info("start")
+    
+		reset_thread = cocotb.start_soon(reset_dut(dut.rst_n, duration_ns=500))    
+		clk_thread = cocotb.start_soon(generate_clock(dut))  # run the clock "in the background"
+		clk2_thread = cocotb.start_soon(generate_clock2(dut))  # run the clock2 "in the background"
+		await Timer(100000, units="ns")
+#    cocotb.triggers.ClockCycles(signal, num_cycles, rising=True)
+#		await Timer(40, units="ns")	
+#		dut.INPUT.value = 1
+#		await Timer(1, units="ms")
+#		assert dut.OUTPUT.value == 0
