446b11c8d9c7167b8840f2c696c50dcf2a2c1bfd
pacoveirano <139478119+pacoveirano@users.noreply.github.com>
2023-09-01 19:12:16 -0300

    change test

--- src/test.py
+++ src/test.py
@@ -4,26 +4,29 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 # https://docs.cocotb.org/en/stable/quickstart.html
 
+clk_T = 2 # in ns
+clk2_T = 1 # in ns
+
 async def generate_clock(dut):
-		for cycle in range(1000):
-				"""Generate clock 2 pulses."""
+		while True:
+				"""Generate clock pulses."""
 				dut.clk.value = 0
-				await Timer(90, units="ns")
+				await Timer(clk_T/2, units="ns")
 				dut.clk.value = 1
-				await Timer(90, units="ns")	
+				await Timer(clk_T/2, units="ns")	
 	
 	
 async def generate_clock2(dut):
-		for cycle in range(1000):
+		while True:
 				"""Generate clock 2 pulses."""
 				dut.clk_2.value = 0
-				await Timer(40, units="ns")
+				await Timer(clk2_T/2, units="ns")
 				dut.clk_2.value = 1
-				await Timer(40, units="ns")	
+				await Timer(clk2_T/2, units="ns")	
 
-async def reset_dut(reset_n, duration_ns):
+async def reset_dut(reset_n):
 		reset_n.value = 0
-		await Timer(duration_ns, units="ns")
+		await Timer(2*clk_T, units="ns")
 		reset_n.value = 1
 		reset_n._log.debug("Reset complete")
 
@@ -31,13 +34,29 @@ async def reset_dut(reset_n, duration_ns):
 @cocotb.test()
 async def test_my_design(dut):
 		dut._log.info("start")
-    
-		reset_thread = cocotb.start_soon(reset_dut(dut.rst_n, duration_ns=500))    
+		dut.sel.value = 0
+		dut.ena.value = 0
+		dut.stb.value = 0
+		dut.uio_in.value = 0b00000000 				  
 		clk_thread = cocotb.start_soon(generate_clock(dut))  # run the clock "in the background"
 		clk2_thread = cocotb.start_soon(generate_clock2(dut))  # run the clock2 "in the background"
-		await Timer(100000, units="ns")
-#    cocotb.triggers.ClockCycles(signal, num_cycles, rising=True)
-#		await Timer(40, units="ns")	
-#		dut.INPUT.value = 1
-#		await Timer(1, units="ms")
-#		assert dut.OUTPUT.value == 0
+		await cocotb.start(reset_dut(dut.rst_n))  
+		dut.ena.value = 1
+		await cocotb.triggers.ClockCycles(dut.clk, 5, rising=True)
+		dut.ena.value = 1
+		dut.sel.value = 0
+		dut.uio_in.value = 0b01010101 
+		await cocotb.triggers.ClockCycles(dut.clk, 5, rising=True)
+		dut.sel.value = 1
+		dut.uio_in.value = 0b11111111 
+		await cocotb.triggers.ClockCycles(dut.clk, 5, rising=True)
+		dut.sel.value = 2
+		dut.uio_in.value = 0b00000000 
+		await cocotb.triggers.ClockCycles(dut.clk, 3, rising=True)
+		dut.sel.value = 3
+		dut.uio_in.value = 0b11111111 
+		await cocotb.triggers.ClockCycles(dut.clk, 3, rising=True)
+		dut.stb.value = 1
+		await cocotb.triggers.ClockCycles(dut.clk, 1, rising=True)
+		dut.stb.value = 0
+		await cocotb.triggers.ClockCycles(dut.clk, 50, rising=True)
